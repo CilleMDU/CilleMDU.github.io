2c95c09c405909fcd7e684c38ef8713406c4dbb8
CilleMDU <eaa25cesc@students.eaaa.dk>
2026-08-11 13:25:01 +0200

    testing deploy

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,2 +1,10 @@
 import { Link } from "react-router";
 import projects from "../data/projects";
+
+export default function HomePage() {
+    return (
+        <div className="home-page">
+            <h1>Welcome to My Portfolio</h1>
+            </div>
+    );
+}
