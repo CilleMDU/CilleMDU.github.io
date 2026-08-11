96eb3e0d60d36492508bde4d05824e6abbd77b3d
CilleMDU <eaa25cesc@students.eaaa.dk>
2026-08-11 17:41:11 +0200

    deployment fix, again

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: process.env.NODE_ENV === "production" ? "/" : "/",
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    sourcemap: false,
+  },
 });
