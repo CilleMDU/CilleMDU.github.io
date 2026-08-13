4d18d54465a9b68dad90249c03ffb720dde894bf
CilleMDU <eaa25cesc@students.eaaa.dk>
2026-08-13 18:27:43 +0200

    footer complete

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,8 @@
 import styles from './Footer.module.css';
 import logo from "../../img/logo.svg";
+import InstaIcon from "../../img/footer/insta.svg";
+import LinkedInIcon from "../../img/footer/linked.svg";
+import GitHubIcon from "../../img/footer/github.svg";
 
 export default function Footer() {
   return (
@@ -23,11 +26,20 @@ export default function Footer() {
         <div className={styles.socialContainer}>
           <h3 className={styles.socialTitle}>Follow Me!</h3>
           <div className={styles.socialIcons}>
+            <a href="https://www.linkedin.com/in/cecilie-schmidt-90a040428/" target="_blank" rel="noopener noreferrer">
+              <img src={LinkedInIcon} alt="LinkedIn" className={styles.socialIcon} />
+            </a>
+            <a href="https://github.com/CilleMDU" target="_blank" rel="noopener noreferrer">
+              <img src={GitHubIcon} alt="GitHub" className={styles.socialIcon} />
+            </a>
+            <a href="https://www.instagram.com/c.schmidt_media/" target="_blank" rel="noopener noreferrer">
+              <img src={InstaIcon} alt="Instagram" className={styles.socialIcon} />
+            </a>
           </div>
         </div>
         <div className={styles.logoContainer}>
           <img src={logo} alt="Logo" className={styles.logo} />
-          <p className={styles.logoText}>Cecilie Schmidt</p>
+          <p className={styles.logoText}>C. Schmidt</p>
         </div>
       </div>
     </footer>
