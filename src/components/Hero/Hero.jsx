331e877923e1613c3ee03aa37d88ac9a9d003a9e
CilleMDU <eaa25cesc@students.eaaa.dk>
2026-08-13 18:54:21 +0200

    hero update

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,30 @@
 import styles from './Hero.module.css';
 import image from '../../img/placeholder/placeholderHero.png';
+import ProjectsInactive from '../../img/clouds/navbarClouds/inactive/projects.svg';
+import ProjectsActive from '../../img/clouds/navbarClouds/active/projectsActive.svg';
+import AboutInactive from '../../img/clouds/navbarClouds/inactive/about.svg';
+import AboutActive from '../../img/clouds/navbarClouds/active/aboutActive.svg';
 
 export default function Hero() {
+
+    const handleProjectsPageClick = () => {
+        window.location.href = '/projects';
+    };
+
+    const handleAboutPageClick = () => {
+        window.location.href = '/about';
+    }
+
     return (
         <section className={styles.hero}>
             <div className={styles.heroContent}>
                 <img src={image} alt="Hero" className={styles.heroImage} />
+                <div className={styles.ctaAbout}>
+                    <img src={AboutInactive} alt="About" className={styles.ctaAboutIcon} onClick={handleAboutPageClick} />
+                </div>
+                <div className={styles.ctaProjects}>
+                    <img src={ProjectsInactive} alt="Projects" className={styles.ctaProjectsIcon} onClick={handleProjectsPageClick} />
+                </div>
             </div>
         </section>
     );
